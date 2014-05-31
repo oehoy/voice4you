f55251c3b060c0cfb5bc340777f878e759e3a721
Oehoy <popov.md5@gmail.com>
2014-05-31 19:58:31 +0400

    Wiki download! F shit! =)

--- wikipedia/wiki.py
+++ wikipedia/wiki.py
@@ -4,8 +4,19 @@ import wikipedia
 
 def main():
 	wikipedia.set_lang("ru")
-	text = wikipedia.page("Глагол")
-	print text.content
+	while 1:
+		comand = raw_input("Введите название статьи: ")
+		if (comand != "exit"):
+			try:
+				text = wikipedia.page(comand)
+				print text.content
+				print "*"*100+"\n\n"
+			except:
+				print "Статья не найдена"
+				print "*"*100+"\n\n"
+		elif (comand == "exit") or (comand== "quit") or(comand== "q"):
+			break
+	
 	return 0
 
 if __name__ == '__main__':
